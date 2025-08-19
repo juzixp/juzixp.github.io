0e7fcddd565ceb85ecfbe3e16f9c3c0cb6043530
juzixp <juzixp@sdod.eu.org>
2025-08-19 08:24:10 +0000

    111 UTC 2025-08-19 Tuesday 08:24:10 Updated By Github Actions

---
+++ js/shares.js
@@ -0,0 +1,1 @@
+var shareIcon=document.querySelector("#share-icon"),shareContent=document.querySelector(".share-content");function openOrHideShareContent(){shareContent.classList.contains("hidden")?shareContent.classList.remove("hidden"):shareContent.classList.add("hidden")}shareIcon&&shareIcon.addEventListener("click",openOrHideShareContent,!1);
